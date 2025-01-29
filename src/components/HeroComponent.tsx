ace7bb26b5c0de6efcee13d43e27be68e9c08388
Hoomam <hoaz1600@student.miun.se>
2025-01-29 21:44:55 +0100

    Fixed image import to Hero

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,6 +1,8 @@
+import cinema from "../assets/cinema.jpg"
+
 const HeroComponent = () => {
   return (
-    <img src="src\assets\cinema.jpg" alt="Cinema production image" style={{ width: '100%', height: 'auto' }} />
+    <img src={cinema} alt="Cinema production image" style={{ width: '100%', height: 'auto' }} />
   )
 }
 
